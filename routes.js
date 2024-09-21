8387f88dd2cd0f286142ef009a607ee33a0297d3
Tartakovskyi Artem <artem.tartakovskiy@gmail.com>
2024-09-22 00:10:26 +0200

    Added views

--- routes.js
+++ routes.js
@@ -5,39 +5,63 @@ const Event = require("./models/Event");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const events = await Event.find();
-  res.render("events", { events });
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = 16;
+    const skip = (page - 1) * limit;
+    const totalEvents = await Event.countDocuments();
+    const events = await Event.find().limit(limit).skip(skip);
+    const totalPages = Math.ceil(totalEvents / limit);
+    res.render("events", {
+      events,
+      currentPage: page,
+      totalPages,
+    });
+  } catch (err) {
+    res.status(500).send("Error retrieving events");
+  }
 });
 
 router.get("/:eventId/register", async (req, res) => {
-  const eventId = req.params.eventId;
-  const event = await Event.findById(eventId);
-  res.render("registration", { event });
+  try {
+    const eventId = req.params.eventId;
+    const event = await Event.findById(eventId);
+    res.render("registration", { event });
+  } catch (err) {
+    res.status(500).send("Error accessing register");
+  }
 });
 
 router.post("/:eventId/register", async (req, res) => {
-  const eventId = req.params.eventId;
-  const { fullName, email, dateOfBirth, heardAbout } = req.body;
+  try {
+    const eventId = req.params.eventId;
+    const { fullName, email, dateOfBirth, heardAbout } = req.body;
 
-  const participant = new Participant({
-    eventId,
-    fullName,
-    email,
-    dateOfBirth: new Date(dateOfBirth),
-    heardAbout,
-  });
+    const participant = new Participant({
+      eventId,
+      fullName,
+      email,
+      dateOfBirth: new Date(dateOfBirth),
+      heardAbout,
+    });
 
-  await participant.save();
-  res.redirect(`/${eventId}/view`);
+    await participant.save();
+    res.redirect(`/${eventId}/view`);
+  } catch (err) {
+    res.status(500).send("Error inserting data");
+  }
 });
 
 router.get("/:eventId/view", async (req, res) => {
-  console.log(req.params.eventId);
-  const eventId = req.params.eventId;
-  const event = await Event.findById(eventId);
-  const participants = await Participant.find({ eventId });
+  try {
+    const eventId = req.params.eventId;
+    const event = await Event.findById(eventId);
+    const participants = await Participant.find({ eventId });
 
-  res.render("viewParticipants", { event, participants });
+    res.render("viewParticipants", { event, participants });
+  } catch (err) {
+    res.status(500).send("Error retrieving participants");
+  }
 });
 
 module.exports = router;
